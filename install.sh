c67a654d17b3fbee51d3e83ba561a92e917f7f07
Mehdi Bounya <ebou-nya@student.1337.ma>
2019-06-16 19:25:30 +0100

    Improve install.sh output

--- install.sh
+++ install.sh
@@ -4,8 +4,11 @@ CUSTOMRC=".spoodyrc"
 TARGETDIR=$HOME"/.prep"
 
 echo "Installing..."
+rm -rf $TARGETDIR
 git clone https://github.com/mehdibo/1337-prepsession.git $TARGETDIR
 
-echo "Add the following line:"
+echo "\n\n\n\n"
+
+echo "Add the following line:\n"
 echo "source "$TARGETDIR"/.rc"
-echo "To "$HOME"/.zshrc"
+echo "\nTo your .zshrc"
