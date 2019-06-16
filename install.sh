77c0c4d9344037dd01e19acb9b8fb2da6f7e9fe2
Mehdi Bounya <ebou-nya@student.1337.ma>
2019-06-16 19:32:23 +0100

    Create brew.packages if doesn't exist

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ echo "Installing..."
 rm -rf $TARGETDIR
 git clone https://github.com/mehdibo/1337-prepsession.git $TARGETDIR
 
+touch $TARGETDIR"/brew.packages"
+
 echo "\n\n\n\n"
 
 echo "Add the following line:\n"
